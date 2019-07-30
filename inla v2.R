e4ec6dfb8e8f6a59b00065256d540c4efe85ce03
osymandius <bolistevens@gmail.com>
2019-07-30 16:21:43 +0100

    mods

--- inla v2.R
+++ inla v2.R
@@ -6,26 +6,26 @@ library(reshape2)
 library(survival)
 
 setwd("~/Documents/GitHub/subnat_fertility")
-load("~/Documents/GitHub/subnat_fertility/asfr.Rda")
+load("~/Documents/GitHub/subnat_fertility/asfr_singleage.Rda")
 
 set_rdhs_config(email="o.stevens@imperial.ac.uk", project="Subnational fertility", config_path = "~/.rdhs.json")
 
 ##+ datasets
-surveys <- dhs_surveys(countryIds = "MW", surveyYearStart=1995)
-ird <- dhs_datasets(fileType = "IR", fileFormat = "flat", surveyIds = surveys$SurveyId)
-ird$path <- unlist(get_datasets(ird))
-
-
-ir <- lapply(ird$path, readRDS) %>%
-  Map(data.frame,
-      surveyid = surveys$SurveyId,
-      country = surveys$CountryName,
-      survyear = surveys$SurveyYear,
-      survtype = surveys$SurveyType,
-      .,
-      stringsAsFactors = FALSE)
-
-tips_surv <- list("DHS" = c(0, 7), "MIS" = c(0, 5))[surveys$SurveyType]
+# surveys <- dhs_surveys(countryIds = "MW", surveyYearStart=1995)
+# ird <- dhs_datasets(fileType = "IR", fileFormat = "flat", surveyIds = surveys$SurveyId)
+# ird$path <- unlist(get_datasets(ird))
+# 
+# 
+# ir <- lapply(ird$path, readRDS) %>%
+#   Map(data.frame,
+#       surveyid = surveys$SurveyId,
+#       country = surveys$CountryName,
+#       survyear = surveys$SurveyYear,
+#       survtype = surveys$SurveyType,
+#       .,
+#       stringsAsFactors = FALSE)
+# 
+# tips_surv <- list("DHS" = c(0, 7), "MIS" = c(0, 5))[surveys$SurveyType]
 # 
 # tfr <- Map(calc_tfr, ir,
 #            by = list(~surveyid + country + survyear + v025),
@@ -40,21 +40,12 @@ tips_surv <- list("DHS" = c(0, 7), "MIS" = c(0, 5))[surveys$SurveyType]
 #   mutate(lower = tfr - qnorm(0.975) * se_tfr,
 #          upper = tfr + qnorm(0.975) * se_tfr)
 # 
-# asfr <- Map(calc_asfr, ir,
-#               by = list(~surveyid + country + survyear+ v025),
-#               tips = tips_surv,
-#               agegr= 15:50,
-#               period = list(1995:2017),
-#               counts = TRUE)
-debugonce(demog_pyears)
-asfr <- Map(calc_asfr, ir,
-            by = list(~surveyid + country + survyear+ v025),
-            tips = tips_surv,
-            agegr= 15:50,
-            period = list(2000:2002),
-            counts = TRUE)
-
-debugonce(calc_asfr)
+# asfr <- Map(calc_asfr1, ir,
+#             by = list(~surveyid + country + survyear+ v025),
+#             tips = tips_surv,
+#             agegr= list(15:50),
+#             period = list(1995:2017),
+#             counts = TRUE)
 
 asfr1 <- asfr %>%
   bind_rows %>%
@@ -68,19 +59,7 @@ asfr1 <- asfr %>%
          id.agegr.period = group_indices(., period, agegr),
          id = 1:nrow(.))
 
-asfr_pred <- asfr %>%
-  bind_rows %>%
-  type.convert %>%
-  mutate(v025 = factor(v025, levels= c(1,2), labels = c("Urban", "Rural"))) %>%
-  filter(period <= survyear) %>%
-  mutate(births = NA_integer_,
-         pys = 1,
-         asfr = NA_integer_,
-         se_asfr = NA_integer_)%>%
-  select("v025", "agegr", "period", "pys") %>%
-  group_by_all() %>%
-  summarise() %>%
-  ungroup() %>%
+asfr_pred <- crossing(v025 = asfr1$v025, period = asfr1$period, agegr = asfr1$agegr, pys=1) %>%
   bind_rows(asfr %>%
               bind_rows %>%
               type.convert %>%
@@ -93,34 +72,83 @@ asfr_pred <- asfr %>%
          id.agegr.period = group_indices(., period, agegr),
          id = 1:nrow(.))
 
+# asfr_pred <- asfr %>%
+#   bind_rows %>%
+#   type.convert %>%
+#   mutate(v025 = factor(v025, levels= c(1,2), labels = c("Urban", "Rural"))) %>%
+#   filter(period <= survyear) %>%
+#   mutate(births = NA_integer_,
+#          pys = 1,
+#          asfr = NA_integer_,
+#          se_asfr = NA_integer_) %>%
+#   select("v025", "agegr", "period", "pys") %>%
+#   group_by_all() %>%
+#   summarise() %>%
+#   ungroup() %>%
+#   bind_rows(asfr %>%
+#               bind_rows %>%
+#               type.convert %>%
+#               mutate(v025 = factor(v025, levels= c(1,2), labels = c("Urban", "Rural"))) %>%
+#               filter(period <= survyear)) %>%
+#   mutate(id.period = group_indices(., period),
+#          id.period2 = id.period,
+#          id.agegr = group_indices(., agegr),
+#          id.agegr2 = id.agegr,
+#          id.agegr.period = group_indices(., period, agegr),
+#          id = 1:nrow(.))
+
 # next things to do:
 #   - Make the `agegr x period` term a RW2 on period (instead of iid). I think the way to do this with `f(id.agegr.period, model = "rw2", group = agegr)` or something like that.
 # - Stratify the data by single-year of age and make a RW2 over age
 # - Fit an interaction of RW2 on age and RW2 on period
 # - Get the district-level data sorted out and start doing space
 
-formula.1 <- births ~ v025 + f(id.agegr.period, model="iid") + f(id.agegr, model="rw1", group=id.period, control.group=list(model="rw2"))
+formula.0 <- births ~ v025 + f(id.agegr.period, model="rw2") + f(id.agegr, model="rw2", group=id.period, control.group=list(model="rw2"))
+formula.1 <- births ~ v025 + f(id.agegr.period, model="rw2", group=id.agegr, control.group=list(model="rw2")) + f(id.agegr, model="rw2", group=id.period, control.group=list(model="rw2"))
 
-mod <- inla(formula.1, family="poisson", data=asfr1, E=pys,
+mod <- inla(formula.1, family="poisson", data=asfr_pred, E=pys,
             control.family=list(link='log'),
             control.predictor=list(compute=TRUE, link=1),
+            control.inla = list(strategy = "gaussian", int.strategy = "eb"),
             control.compute=list(config = TRUE, dic= TRUE, cpo=TRUE))
             # control.fixed=list(mean=0, prec=0.00001,
                               # mean.intercept=0, prec.intercept=0.00001))
 
+save(form0mod, file="form0 mod.Rda")
+load("form0 mod.Rda")
 
-mod$marginals.random$id.agegr %>%
-  lapply(t_emarg)
+form0mod <- mod
 
 asfr_pred %>%
-  filter(id<309) %>%
+  filter(id<1541) %>%
   select("v025", "agegr", "period","pys", "id") %>%
-  left_join(mod$summary.fitted.values[1:308, ] %>%
-              mutate(id = 1:308), by="id") %>%
+  left_join(form1mod$summary.fitted.values[1:1540, ] %>%
+              mutate(id = 1:1540), by="id") %>%
   arrange(period, v025, agegr) %>%
+  mutate(agegroup = rep(1:7, each=5, times=44),
+         agegroup = factor(agegroup, levels=1:7, labels=c("15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49"))) %>%
   ggplot(aes(x=period, y=`0.5quant`, ymin=`0.025quant`, ymax=`0.975quant`, group=agegr))+
-  geom_line(aes(color=agegr)) +
+  geom_line(aes(color=agegroup)) +
+  ylim(0,0.4) +
   facet_wrap(~v025)
+
+foo <- mod$summary.random$id.agegr
+
+mod$marginals.random$id.agegr %>%
+  lapply(t_emarg) %>%
+  melt() %>%
+  mutate(age = rep(15:49, 22),
+         agegroup = rep(1:7, each=5, times=22),
+         agegroup = factor(agegr, labels=c("15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49")),
+         period = rep(1995:2016, each=35)) %>%
+  ggplot(aes(x=period, y=value, group=age)) +
+    geom_line(aes(color=agegr)) +
+    xlab("ASFR") +
+    ylab("")
+
+
+
+
 
 t_emarg <- function(x){
   inla.emarginal(exp, x)
@@ -139,7 +167,7 @@ t_qmarg <- function(x){
 mod$marginals.random$id.agegr %>%
   lapply(t_tmarg) %>%
   melt() %>%
-  mutate(Var2 = as.character(Var2)) %>%
+  mutate(Var2 = as.character(Var2))
   pivot_wider(names_from=Var2) %>%
   mutate(agegr = factor(as.numeric(factor(L1)), labels=unique(asfr1$agegr))) %>%
   ggplot(aes(x=x, y=y, group=agegr)) +
@@ -253,41 +281,3 @@ lapply(x, function(y){exp(inla.hpdmarginal(0.95, y))}) %>%
   pivot_wider(names_from=Var2) %>%
   mutate(L1 = factor(as.numeric(factor(L1)), labels=unique(asfr1$agegr)))
 
-exp.b0.mean <- inla.emarginal(exp, mod$marginals.fixed[[1]])
-exp.b0.CI <- inla.qmarginal(c(0.025, 0.975), inla.tmarginal(exp, mod$marginals.fixed[[1]]))
-exp.b0.CI
-
- mod$summary.fitted.values
-
-n.samples = 1000
-samples = inla.posterior.sample(n.samples, result = mod)
-
-mod$misc$configs$contents
-
-
-round(mod$summary.hyperpar, 3)
-
-  mod2 <- glmer(births ~ agegr + period + v025 + (1 |agegr) , family = poisson, data = asfr2, offset = log(pys))
-  mod2.int <- glmer(births ~ agegr*period + v025 + (1 + period | agegr), family = poisson, data = asfr2, offset = log(pys))
-  janova(mod2.int, mod2)
-  
-  View(coef(mod2.inter))
-  
-  df_pred2 <- crossing(period = 1995:2017,
-                       agegr = levels(asfr$agegr),
-                       pys = 1,
-                       v025 = levels(asfr$v025))
-  
-  pred2 <- predict(mod2.int, newdata = df_pred2)
-  
-  df_pred2 <- data.frame(df_pred2, pred2) %>%
-    mutate(asfr = exp(pred2))
-  
-  ggplot(asfr2, aes(period, asfr, color = surveyid)) +
-    geom_point() +
-    geom_line() +
-    #geom_ribbon(aes(ymin = lower, ymax = upper), data = df_pred, color = NA, fill = "darkred", alpha = 0.3) +
-    geom_line(data = df_pred2, aes(y=asfr), color = "darkred") +
-    # facet_wrap(~v025)
-    facet_grid(v025~agegr)
-  
