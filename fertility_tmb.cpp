03f368b9dd1ff81925dcb2b3ebac6e16e688bad3
osymandius <bolistevens@gmail.com>
2020-02-06 14:13:45 +0000

    NaN troubleshooting

--- fertility_tmb.cpp
+++ fertility_tmb.cpp
@@ -4,39 +4,75 @@ template<class Type>
 Type objective_function<Type>::operator() ()
   
 {
-  DATA_MATRIX(dat_m)
-  DATA_MATRIX(log_offset)
+  
+  DATA_ARRAY(dat_m)
+  DATA_ARRAY(log_offset)
+  DATA_SPARSE_MATRIX(P);
   
   PARAMETER_VECTOR(alpha_a);
   PARAMETER_VECTOR(beta_a);
   PARAMETER_VECTOR(rw_age);
   PARAMETER_VECTOR(rw_time);
-  PARAMETER_MATRIX(rw_interaction);
+  // PARAMETER_MATRIX(rw_interaction);
   PARAMETER(log_prec_epsilon);
   PARAMETER(log_prec_rw_age);
   PARAMETER(log_prec_rw_time);
-  PARAMETER(log_prec_rw_interaction);
+  // PARAMETER(log_prec_rw_interaction);
   
+  PARAMETER_VECTOR(V);
+  PARAMETER_VECTOR(W);
   
-  int num_time = dat_m.rows();
-  int num_age = dat_m.cols();
+  PARAMETER(log_sigma2_V);
+  PARAMETER(log_sigma2_U);
   
   Type nll = 0;
-  matrix<Type> log_mu(num_time, num_age);
+  
+  
+  int num_time = 22;
+  int num_age = 7;
+  int num_dist = 33;
+  
+  
+  Type tau_V = 1 / exp(log_sigma2_V);
+  Type tau_U = 1 / exp(log_sigma2_U);
+  
+  nll -= dgamma(tau_V, Type(0.5), Type(2000), 1);
+  nll -= dgamma(tau_U, Type(0.5), Type(2000), 1);
+  
+  
+  // nll -= dnorm(tau_V, Type(0), Type(10), 1);
+  // nll -= dnorm(tau_U, Type(0), Type(10), 1);
+  
+  nll -= dnorm(V, Type(0), exp(0.5 * log_sigma2_V), 1).sum();
+  
+  
+  // This is the pairwise thing I think??
+  vector<Type> tmp = P * W;
+  nll -= -0.5 * (W * tmp).sum();
+  
+  
+  vector<Type> U = W * exp(0.5 * log_sigma2_U);
+  nll -= dnorm(U.sum(), Type(0), Type(0.00001), 1);
+  
+  
+  array<Type> log_mu(num_time, num_age, num_dist);
   
   nll -= dnorm(alpha_a, Type(0), Type(1), TRUE).sum();
   nll -= dnorm(beta_a, Type(0), Type(1), TRUE).sum();
   nll -= dnorm(log_prec_epsilon, Type(0), Type(1), TRUE);
   nll -= dnorm(log_prec_rw_age, Type(0), Type(1), TRUE);
   nll -= dnorm(log_prec_rw_time, Type(0), Type(1), TRUE);
-  nll -= dnorm(log_prec_rw_interaction, Type(0), Type(1), TRUE);
+  
+  // nll -= dnorm(log_prec_rw_interaction, Type(0), Type(1), TRUE);
   
   // nll -= dlgamma(log_prec_epsilon, Type(1), Type(0.00005), TRUE);
   // nll -= dlgamma(log_prec_nu, Type(1), Type(0.00005), 1); 
   
   for (int t = 0; t < num_time; t++) {
     for(int a = 0; a < num_age; a++) {
-      log_mu(t, a) = alpha_a(a) + beta_a(a)*t + rw_time(t) + rw_age(a) + rw_interaction(t, a);
+      for(int i = 0; i < num_dist; i++) {
+        log_mu(t, a, i) = alpha_a(a) + beta_a(a)*t + rw_time(t) + rw_age(a) + V(i) + U(i);
+      }
     }
   }
   
@@ -44,15 +80,17 @@ Type objective_function<Type>::operator() ()
     nll -= dnorm(rw_age(a), a==0 ? 0 : rw_age(a-1), exp(log_prec_rw_age), TRUE);
   }
   
+  
   for (int t = 0; t < num_time; t++) {
     nll -= dnorm(rw_time(t), t==0 ? 0 : rw_time(t-1), exp(log_prec_rw_time), TRUE);
   }
   
-  for (int t = 0; t < num_time; t++) {
-    for (int a = 0; a < num_age; a++) {
-      nll -= dnorm(rw_interaction(t, a), t==0 ? 0 : rw_interaction(t-1, a), exp(log_prec_rw_interaction), TRUE);
-    }
-  }
+  
+  // for (int t = 0; t < num_time; t++) {
+  //   for (int a = 0; a < num_age; a++) {
+  //     nll -= dnorm(rw_interaction(t, a), t==0 ? 0 : rw_interaction(t-1, a), exp(log_prec_rw_interaction), TRUE);
+  //   }
+  // }
   
   
   
@@ -63,13 +101,20 @@ Type objective_function<Type>::operator() ()
     nll -= dnorm(mu_col, 0, exp(log_prec_epsilon), TRUE).sum();
   }
   
+  
   for (int t = 0; t < num_time; t++) {
     for(int a = 0; a < num_age; a++) {
-      nll -= dpois(dat_m(t, a), exp(log_mu(t, a) + log_offset(t, a)), TRUE);
+      for(int i = 0; i < num_dist; i++) {
+        std::isnan(log_offset(t,a,i)) ? nll-=0 : nll -= dpois(dat_m(t, a, i), exp(log_mu(t, a, i) + log_offset(t, a, i)), TRUE);
+      }
     }
   }
   
-  REPORT(log_mu);
+  
+  REPORT(V);
+  REPORT(W);
+  REPORT(log_sigma2_V);
+  REPORT(log_sigma2_U);
   
   return nll;
   
--- tmb.R
+++ tmb.R
@@ -1,32 +1,65 @@
 library(TMB)
 library(tidyverse)
+library(abind)
+library(sf)
+library(spdep)
 setwd("~/Documents/GitHub/subnat_fertility/")
 
 asfr <- readRDS("~/Documents/GitHub/subnat_fertility/asfr_pred_subnat_15_2020_NEW.rds")[["MWI"]]
+boundaries <- readRDS("~/Documents/GitHub/subnat_fertility/area_boundaries.rds")
+
+iso3_codes <- c("LSO", "MOZ", "MWI", "NAM", "SWZ", "TZA", "UGA", "ZMB", "ZWE")
+
+paths <- paste0("~/Documents/GitHub/naomi-data/", iso3_codes, "/data/", tolower(iso3_codes), "_areas.geojson")
+area_cols <- c("iso3", "area_id", "area_name", "area_level", "parent_area_id", "naomi_level")
+
+areas_long <- lapply(paths, read_sf) %>% lapply(function(x) {
+  iso3_code <- x %>%
+    filter(area_level == 0) %>%
+    select(area_id) %>%
+    unique %>%
+    .$area_id
+  
+  x <- x %>%
+    mutate(iso3 = iso3_code) %>%
+    st_drop_geometry() %>%
+    select(area_cols)
+  
+  return(x)
+}) %>% 
+  bind_rows
 
 dat_m <- asfr %>%
   filter(!is.na(surveyid)) %>%
-  select(period, agegr, births, pys) %>%
-  group_by(period, agegr) %>%
+  select(area_id, period, agegr, births, pys) %>%
+  group_by(period, agegr, area_id) %>%
   summarise_all(sum) %>%
   ungroup %>%
   select(-pys) %>%
   mutate(agegr = as.numeric(factor(agegr))) %>%
   pivot_wider(names_from = agegr, values_from = births) %>%
+  complete(period, area_id, fill=map(asfr[2:9], ~NA)) %>%
   select(-period) %>%
-  as.matrix()
+  nest(-area_id) %>%
+  mutate(data = map(data, ~as.matrix(.x))) %>%
+  pull(data) %>%
+  invoke(abind, ., along=3)
 
 log_offset <- asfr %>%
   filter(!is.na(surveyid)) %>%
-  select(period, agegr, births, pys) %>%
-  group_by(period, agegr) %>%
+  select(area_id, period, agegr, births, pys) %>%
+  group_by(period, agegr, area_id) %>%
   summarise_all(sum) %>%
   ungroup %>%
   select(-births) %>%
   mutate(agegr = as.numeric(factor(agegr)), pys = log(pys)) %>%
   pivot_wider(names_from = agegr, values_from = pys) %>%
+  complete(period, area_id, fill=map(asfr[2:9], ~NA)) %>%
   select(-period) %>%
-  as.matrix()
+  nest(-area_id) %>%
+  mutate(data = map(data, ~as.matrix(.x))) %>%
+  pull(data) %>%
+  invoke(abind, ., along=3)
 
 # dat_m <- asfr %>% 
 #   filter(!is.na(surveyid)) %>%
@@ -49,43 +82,58 @@ log_offset <- asfr %>%
 #   as.matrix()
 
 
-compile("fertility_tmb.cpp")               # Compile the C++ file
-dyn.load(dynlib("fertility_tmb"))  
+sh <- areas_long %>%
+  filter(iso3 == "MWI", naomi_level) %>%
+  mutate(area_idx = row_number())
 
-#alpha_a = rep(1,7), beta_a = rep(1, 7)
-f <-  MakeADFun(data=list(dat_m = dat_m, log_offset = log_offset), 
+#' Neighbor list
+nb <- sh %>%
+  left_join(boundaries) %>%
+  st_as_sf %>%
+  as("Spatial") %>%
+  spdep::poly2nb() %>%
+  `names<-`(sh$area_idx)
+
+adj <- nb2mat(nb, zero.policy=TRUE, style="B")
+diag <- diag(rowSums(adj))
+icar_adj <- diag-adj
+
+
+compile("fertility_tmb.cpp")               # Compile the C++ file
+dyn.load(dynlib("fertility_tmb"))
+
+f <-  MakeADFun(data=list(dat_m = dat_m, log_offset = log_offset, P = as(icar_adj, "dgTMatrix")), 
                 parameters=list(alpha_a = rep(0,7), 
                                 beta_a = rep(0, 7), 
                                 rw_time = rep(0,22), 
                                 rw_age = rep(0,7), 
-                                rw_interaction = matrix(0, 22, 7),
+                                # rw_interaction = matrix(0, 22, 7),
                                 log_prec_epsilon =1,
                                 log_prec_rw_time = 1,
                                 log_prec_rw_age = 1,
-                                log_prec_rw_interaction = 1
+                                V = rep(0, 33), 
+                                W = rep(0, 33), 
+                                log_sigma2_V = 0, 
+                                log_sigma2_U = 0
+                                # log_prec_rw_interaction = 1
                                 ), 
+                # random = c("V", "W"),
                 # map = list(), 
                 DLL = "fertility_tmb")
 
-f$fn(f$par)
-f$report()
+f$env$tracepar <- TRUE
+# f$report()
 
-fit = nlminb(f$par,f$fn,f$gr, control = list(iter.max = 10000, eval.max = 10000))
+fit = nlminb(f$par,f$fn,f$gr, control = list(iter.max = 100000, eval.max = 100000))
 print(fit)
 
 int <- data.frame(agegr = 0:6, intercept = fit$par[1:7], slope = fit$par[8:14])
-
-int %>%
-  bind_rows %>%
-  mutate(period = 1995:2016) %>%
-  melt(id="period", variable.name="agegr") %>%
-  ggplot(aes(x=period, y=value, color=agegr, group=agegr)) +
-    geom_line()
 
 crossing(period = 0:21, agegr = 0:6) %>%
   left_join(int) %>%
   left_join(data.frame(period = 0:21, rw_time = fit$par[names(fit$par) == "rw_time"])) %>%
   left_join(data.frame(agegr= 0:6, rw_age = fit$par[names(fit$par) == "rw_age"])) %>%
+  # left_join(data.frame(period = rep(0:21, each=7), agegr = rep(0:6, times=22), interaction = fit$par[names(fit$par) == "rw_interaction"])) %>%
   mutate(point = exp(intercept + period*slope + rw_time + rw_age)) %>%
   ggplot(aes(x=period, y=point, color=agegr, group=agegr)) +
     geom_line()
