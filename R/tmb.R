5668cfce5a250cc65553fe5756415381decf8b98
osymandius <bolistevens@gmail.com>
2020-02-17 17:48:00 +0000

    push

--- R/tmb.R
+++ R/tmb.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(abind)
 library(sf)
 library(spdep)
+devtools::load_all("~/Documents/GitHub/naomi/")
 
 setwd("~/Documents/GitHub/subnat_fertility/")
 
@@ -33,36 +34,63 @@ areas_long <- lapply(paths, read_sf) %>%
 
 dat_m <- asfr %>%
   filter(!is.na(surveyid)) %>%
-  select(area_id, period, agegr, births, pys) %>%
-  group_by(period, agegr, area_id) %>%
-  summarise_all(sum) %>%
-  ungroup %>%
-  select(-pys) %>%
-  mutate(agegr = as.numeric(factor(agegr))) %>%
-  pivot_wider(names_from = agegr, values_from = births) %>%
-  complete(period, area_id, fill=map(asfr[2:9], ~NA)) %>%
-  select(-period) %>%
-  nest(-area_id) %>%
-  mutate(data = map(data, ~as.matrix(.x))) %>%
-  pull(data) %>%
-  invoke(abind, ., along=3)
+  select(area_id, period, agegr, tips, births, pys) %>%
+  # complete(period, area_id, fill=map(asfr[2:9], ~NA)) %>%
+  mutate(period = group_indices(., period),
+         area_id = group_indices(., area_id),
+         agegr = group_indices(., agegr),
+         pys = log(pys),
+         pys = ifelse(is.na(pys), -999, pys)
+  )
 
-log_offset <- asfr %>%
-  filter(!is.na(surveyid)) %>%
-  select(area_id, period, agegr, births, pys) %>%
-  group_by(period, agegr, area_id) %>%
-  summarise_all(sum) %>%
-  ungroup %>%
-  select(-births) %>%
-  mutate(agegr = as.numeric(factor(agegr)), pys = log(pys)) %>%
-  pivot_wider(names_from = agegr, values_from = pys) %>%
-  complete(period, area_id, fill=map(asfr[3:9], ~NA)) %>%
-  mutate_if(is.numeric , replace_na, replace = -999) %>%
-  select(-period) %>%
-  nest(-area_id) %>%
-  mutate(data = map(data, ~as.matrix(.x))) %>%
-  pull(data) %>%
-  invoke(abind, ., along=3)
+Z_tips <- data.frame(tips_dummy = ifelse(dat_m$tips > 5, 1, 0)) %>%
+  sparse_model_matrix(~0 + tips_dummy, .)
+
+# log_offset <- asfr %>%
+#   filter(!is.na(surveyid)) %>%
+#   select(area_id, period, agegr, tips, births, pys) %>%
+#   group_by(period, agegr, area_id, tips) %>%
+#   summarise_all(sum) %>%
+#   ungroup %>%
+#   select(-births) %>%
+#   complete(period, area_id, fill=map(asfr[3:9], ~NA)) %>%
+#   mutate(period = group_indices(., period),
+#          area_id = group_indices(., area_id),
+#          agegr = group_indices(., agegr)) %>%
+#   mutate_if(is.numeric , replace_na, replace = -999) 
+
+# dat_m <- asfr %>%
+#   filter(!is.na(surveyid)) %>%
+#   select(area_id, period, agegr, tips, births, pys) %>%
+#   group_by(period, agegr, area_id, tips) %>%
+#   summarise_all(sum) %>%
+#   ungroup %>%
+#   select(-pys) %>%
+#   mutate(agegr = as.numeric(factor(agegr))) %>%
+#   pivot_wider(names_from = agegr, values_from = births) %>%
+#   complete(period, area_id, fill=map(asfr[2:9], ~NA)) %>%
+#   select(-period) %>%
+#   nest(-area_id) %>%
+#   mutate(data = map(data, ~as.matrix(.x))) %>%
+#   pull(data) %>%
+#   invoke(abind, ., along=3)
+
+# log_offset <- asfr %>%
+#   filter(!is.na(surveyid)) %>%
+#   select(area_id, period, agegr, births, pys) %>%
+#   group_by(period, agegr, area_id) %>%
+#   summarise_all(sum) %>%
+#   ungroup %>%
+#   select(-births) %>%
+#   mutate(agegr = as.numeric(factor(agegr)), pys = log(pys)) %>%
+#   pivot_wider(names_from = agegr, values_from = pys) %>%
+#   complete(period, area_id, fill=map(asfr[3:9], ~NA)) %>%
+#   mutate_if(is.numeric , replace_na, replace = -999) %>%
+#   select(-period) %>%
+#   nest(-area_id) %>%
+#   mutate(data = map(data, ~as.matrix(.x))) %>%
+#   pull(data) %>%
+#   invoke(abind, ., along=3)
 
 sh <- areas_long %>%
   filter(iso3 == "MWI", naomi_level) %>%
@@ -84,23 +112,26 @@ Q <- INLA::inla.scale.model(diag(rowSums(adj)) - adj,
 compile("tmb/fertility_tmb.cpp")               # Compile the C++ file
 dyn.load(dynlib("tmb/fertility_tmb"))
 
-f <-  MakeADFun(data=list(dat_m = dat_m, 
-                          log_offset = log_offset, 
+f <-  MakeADFun(data=list(dat_m = as.vector(dat_m$births), 
+                          log_offset = as.vector(dat_m$pys), 
                           Q = Q,
+                          Z_tips = Z_tips
                           # Z_i = naomi::sparse_model_matrix(~0 + unique(area_id), asfr)
                           ), 
                 parameters=list(alpha = 0,
                                 rw_time = rep(0,22), 
                                 rw_age = rep(0,7), 
-                                log_prec_epsilon = 0,
-                                log_prec_rw_time = 0,
-                                log_prec_rw_age = 0,
-                                logit_U_str = rep(0,33),
+                                rw_tips = rep(0, 15),
+                                log_epsilon = 0,
+                                log_sigma_rw_age = 0,
+                                log_sigma_rw_time = 0,
+                                log_sigma_rw_tips = 0,
+                                U_str = rep(0,33),
                                 U_iid = rep(0,33), 
-                                log_U_sigma = 0, # Variance
+                                log_sigma_U = 0, # Variance
                                 logit_U_rho = 0 # Share of variance between structured and unstructured components
                                 ), 
-                random = c("log_U_sigma", "logit_U_rho"),
+                random = c("log_sigma_U", "logit_U_rho"),
                 DLL = "fertility_tmb",
                 hessian = FALSE,
                 checkParameterOrder=FALSE)
--- tmb/fertility_tmb.cpp
+++ tmb/fertility_tmb.cpp
@@ -6,22 +6,26 @@ Type objective_function<Type>::operator() ()
 {
   Type nll = 0;
   
-  DATA_ARRAY(dat_m)
-  DATA_ARRAY(log_offset)
+  DATA_VECTOR(dat_m)
+  DATA_VECTOR(log_offset)
   DATA_SPARSE_MATRIX(Q);
+  DATA_SPARSE_MATRIX(Z_tips);
   // DATA_SPARSE_MATRIX(Z_i);
   
   PARAMETER(alpha);
   PARAMETER_VECTOR(rw_age);
   PARAMETER_VECTOR(rw_time);
-  PARAMETER(log_prec_epsilon);
-  PARAMETER(log_prec_rw_age);
-  PARAMETER(log_prec_rw_time);
+  PARAMETER_VECTOR(rw_tips);
+  
+  PARAMETER(log_epsilon);
+  PARAMETER(log_sigma_rw_age);
+  PARAMETER(log_sigma_rw_time);
+  PARAMETER(log_sigma_rw_tips);
   
   //////// SPATIAL MODEL ////////
   
-  PARAMETER_VECTOR(logit_U_str); // ICAR
-  vector<Type> U_str = exp(logit_U_str)/(1+exp(logit_U_str));
+  PARAMETER_VECTOR(U_str); // ICAR
+  // vector<Type> U_str = exp(logit_U_str)/(1+exp(logit_U_str));
   
   nll -= Type(-0.5) * (U_str * (Q * U_str)).sum(); // I don't understand this line. Pairwise? But how?
   nll -= dnorm(U_str.sum(), Type(0), Type(0.001) * U_str.size(), 1); // sum to zero constraint
@@ -35,9 +39,9 @@ Type objective_function<Type>::operator() ()
   nll -= dbeta(U_rho, Type(0.5), Type(0.5), true);
 
   
-  PARAMETER(log_U_sigma);
-  Type U_sigma = exp(log_U_sigma);
-  nll -= dnorm(U_sigma, Type(0), Type(2.5), true) + log_U_sigma;
+  PARAMETER(log_sigma_U);
+  Type U_sigma = exp(log_sigma_U);
+  nll -= dnorm(U_sigma, Type(0), Type(2.5), true) + log_sigma_U;
   
   // vector<Type> spatial = U_sigma*(sqrt(1 - U_rho) * U_iid + sqrt(U_rho) * U_str);
   
@@ -51,36 +55,42 @@ Type objective_function<Type>::operator() ()
   array<Type> log_mu(num_time, num_age, num_dist);
   
   nll -= dnorm(alpha, Type(0), Type(1), TRUE);
-  nll -= dnorm(log_prec_epsilon, Type(0), Type(1), TRUE);
-  nll -= dnorm(log_prec_rw_age, Type(0), Type(1), TRUE);
-  nll -= dnorm(log_prec_rw_time, Type(0), Type(1), TRUE);
+  nll -= dnorm(log_epsilon, Type(0), Type(1), TRUE);
+  nll -= dnorm(log_sigma_rw_age, Type(0), Type(1), TRUE);
+  nll -= dnorm(log_sigma_rw_time, Type(0), Type(1), TRUE);
   
   
-  for (int t = 0; t < num_time; t++) {
-    for(int a = 0; a < num_age; a++) {
-      for(int i = 0; i < num_dist; i++) {
-        log_mu(t, a, i) = alpha + rw_time(t) + rw_age(a) + U_sigma*(
-          sqrt(1 - U_rho) * U_iid(i) + 
-          sqrt(U_rho) * U_str(i)
-        );
-      }
-    }
-  }
+  // for (int t = 0; t < num_time; t++) {
+  //   for(int a = 0; a < num_age; a++) {
+  //     for(int i = 0; i < num_dist; i++) {
+  //       for(int tips = 0; tips <15; tips ++) {
+  //         log_mu(t, a, i) = alpha + rw_tips(tips) + rw_time(t) + rw_age(a) + U_sigma*(
+  //           sqrt(1 - U_rho) * U_iid(i) + 
+  //           sqrt(U_rho) * U_str(i)
+  //         );
+  //       }
+  //     }
+  //   }
+  // }
   
   for (int a = 0; a < num_age; a++) {
-    nll -= dnorm(rw_age(a), a==0 ? 0 : rw_age(a-1), exp(log_prec_rw_age), TRUE);
+    nll -= dnorm(rw_age(a), a==0 ? 0 : rw_age(a-1), exp(log_sigma_rw_age), TRUE);
   }
   
   
   for (int t = 0; t < num_time; t++) {
-    nll -= dnorm(rw_time(t), t==0 ? 0 : rw_time(t-1), exp(log_prec_rw_time), TRUE);
+    nll -= dnorm(rw_time(t), t==0 ? 0 : rw_time(t-1), exp(log_sigma_rw_time), TRUE);
+  }
+  
+  for (int tips = 0; tips < 15; tips++) {
+    nll -= dnorm(rw_tips(tips), tips==0 ? 0 : rw_tips(tips-1), exp(log_sigma_rw_tips), TRUE);
   }
 
   // This is probably wrong and/or the wrong way to do this.
   vector<Type> mu_col(num_time);
   for (int y = 0; y<num_age; y++) {
     mu_col = log_mu.col(y);
-    nll -= dnorm(mu_col, 0, exp(log_prec_epsilon), TRUE).sum();
+    nll -= dnorm(mu_col, 0, exp(log_epsilon), TRUE).sum();
   }
 
   
@@ -94,7 +104,7 @@ Type objective_function<Type>::operator() ()
     }
   }
   
-  ADREPORT(log_mu);
+  // ADREPORT(log_mu);
   
   return nll;
   
